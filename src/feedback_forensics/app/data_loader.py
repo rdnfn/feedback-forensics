5cfa1897cfd51404fab2ae1d70c27d75c8254551
rdnfn <75615911+rdnfn@users.noreply.github.com>
2025-02-19 14:13:37 +0000

    improve data loader

--- src/feedback_forensics/app/data_loader.py
+++ src/feedback_forensics/app/data_loader.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 import pathlib
 from loguru import logger
 
@@ -19,15 +20,34 @@ def clone_repo(username, token, repo_name, clone_directory):
     clone_directory (str): Local directory where the repository should be cloned
     """
     # ensure the clone directory exists
+    dir_exists = os.path.exists(clone_directory / repo_name)
+
+    if dir_exists:
+        logger.info(
+            f"Standard data repository '{repo_name}' already exists in '{clone_directory}'. Skipping loading data from repo. Delete directory to re-clone standard data."
+        )
+        return None
+
     pathlib.Path(clone_directory).mkdir(parents=True, exist_ok=True)
 
     # Form the complete GitHub URL with credentials
-    git_url = f"https://{username}:{token}@github.com/{username}/{repo_name}.git"
 
-    # Execute the git clone command
-    subprocess.run(
-        [f"git clone {git_url}"], shell=True, check=True, cwd=clone_directory
-    )
+    # Form the complete GitHub URL with credentials
+    if token:
+        logger.info("Using token and https to clone data from repository.")
+        git_url = f"https://{username}:{token}@github.com/{username}/{repo_name}.git"
+        # Execute the git clone command
+        subprocess.run(
+            [f"git clone {git_url}"], shell=True, check=True, cwd=clone_directory
+        )
+    else:
+        # try via ssh
+        logger.info("Using SSH to clone data from repository.")
+        git_url = f"git@github.com:{username}/{repo_name}.git"
+        subprocess.run(
+            [f"git clone {git_url}"], shell=True, check=True, cwd=clone_directory
+        )
+    logger.info("Data loaded from repo successfully.")
 
 
 def load_icai_data():
@@ -45,7 +65,7 @@ def load_icai_data():
     try:
         # Clone the repository
         clone_repo(username, GITHUB_TOKEN, repo_name, clone_directory)
-
-        logger.info("Data loaded from repo successfully.")
     except Exception as e:
-        logger.error(f"Error loading data from repo: {e}")
+        logger.warning(
+            f"Failed to load standard data from repo (error: '{e}'). Skipping this step."
+        )
