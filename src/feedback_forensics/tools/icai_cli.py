1d02fe09a0c9e765298317ce038fb2b8b6c9b54a
rdnfn <75615911+rdnfn@users.noreply.github.com>
2025-05-29 01:15:25 +0200

    Add output dir to annotation CLI

--- src/feedback_forensics/tools/icai_cli.py
+++ src/feedback_forensics/tools/icai_cli.py
@@ -25,6 +25,13 @@ def run():
         default="v4",
         help="Version of standard principles to test (default: v2)",
     )
+    parser.add_argument(
+        "--output-dir",
+        "-o",
+        type=str,
+        default=None,
+        help="Path to output directory (default: exp/outputs/DATETIME)",
+    )
 
     args = parser.parse_args()
 
@@ -40,6 +47,9 @@ def run():
         "annotator.skip=true",
         "s0_skip_principle_generation=true",
     ]
+
+    if args.output_dir is not None:
+        icai_cmd.append(f'hydra.run.dir="{args.output_dir}"')
 
     cmd_str = " ".join(icai_cmd)
 
